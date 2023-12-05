48e43ca00bfb54546e50db2711ab6046a9c8e9e6
Nhật Dương <duongintech@gmail.com>
2023-12-05 10:33:43 +0700

    full code

---
+++ form/src/components/Form.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import SignUp from "./SignUp";
+import SignUpSuccess from "./SignUpSuccess";
+
+const Form = () => {
+    const [formIsSubmitted, setFormIsSubmitted] = useState(false);
+    const submitForm = () => {
+        setFormIsSubmitted(true);
+    }
+    return (
+        <div>
+            {!formIsSubmitted ? <SignUp submitForm={ submitForm} /> : <SignUpSuccess/>}
+        </div>
+    );
+};
+
+export default Form;
---
+++ form/src/components/SignUpSuccess.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../App.css";
+
+const SignUpSuccess = () => {
+    return (
+        <div className="container">
+            <div className="app-wapper">
+                <h1 className="form-success">Account Created!</h1>
+            </div>
+        </div>
+    );
+};
+
+export default SignUpSuccess;
